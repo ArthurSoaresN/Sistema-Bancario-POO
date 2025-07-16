47ce2291beadf39ddba128fe90b2af56a94ca716
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-15 23:46:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -105,3 +105,19 @@ class ContaCorrente(Conta):
             C/C: {self._numero}
             Titular: {self._cliente.nome}"""
     
+class Historico:
+    def __init__(self):
+        self._transacoes = []
+    
+    @property
+    def transacoes(self):
+        return self._transacoes
+
+    def adicionar_transacao(self, transacao):
+        self._transacoes.append(
+            {
+                "tipo": transacao.__class__.__name___,
+                "valor": transacao.valor,
+                "data": datetime.now().strftime("%d/%m /%Y, %H:%M")
+            }
+        )
