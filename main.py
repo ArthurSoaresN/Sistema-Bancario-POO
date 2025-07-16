34d6267890e524eda379eb33151a08d9897166d9
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-15 23:25:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -26,4 +26,51 @@ class Conta:
         self._agencia = "0001"
         self._cliente = cliente
         self._historico = Historico()
-        
+
+@classmethod
+def nova_conta(cls, cliente, numero):
+        return cls(numero, cliente)
+
+@property
+def saldo(self):
+    return self._saldo
+
+@property
+def numero(self):
+    return self._numero
+
+@property
+def agencia(self):
+    return self._agencia
+
+@property
+def cliente(self):
+    return self._cliente
+
+@property
+def historico(self):
+    return self._historico
+
+def sacar(self, valor):
+    saldo = self.saldo
+    excedeu_saldo = valor > saldo
+
+    if excedeu_saldo:
+        print("Saldo Insuficente")
+    elif valor > 0:
+        self._saldo -= valor
+        print("Saque Realizado")
+        return True
+    else:
+        print("Valor Invalido")
+    
+    return False
+
+def depositar(self, valor):
+    if valor > 0:
+        self._saldo += valor
+        print("Deposito Realizado")
+        return True
+    else:
+        print("Operação Falhou")
+        return False   
